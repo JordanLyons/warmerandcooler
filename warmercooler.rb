b8a2628b5f1b2bc0f0fdf2dd6dc9f74848761acd
Jordan Lyons <JordanLyons@gmail.com>
2017-04-09 11:42:01 -0600

    Remove first guess from while loop

--- warmercooler.rb
+++ warmercooler.rb
@@ -3,9 +3,9 @@ guesses = []
 puts solution # This statement is temporary while I test the game.
 puts "Guess what number I'm thinking of. It's between 1 and 1000"
 guess = gets.to_i
+puts "Wrong! Guess again!" if guess != solution
 while guess != solution
     guesses.push(guess)
-    puts "Wrong! Guess again!"
     puts "You've guessed #{guesses}" # Temporary, to be replaced by prompt comparing guesses.
     guess = gets.to_i
 end 
