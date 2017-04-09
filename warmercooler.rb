1310732a75efdb5a56a81b9caeab708bb1d31c8f
Jordan Lyons <JordanLyons@gmail.com>
2017-04-09 11:16:07 -0600

    Rename 'answer' variable to 'solution'

--- warmercooler.rb
+++ warmercooler.rb
@@ -1,8 +1,9 @@
-answer = 1 + rand(1000)
-puts answer # This statement is temporary while I test the game.
+solution = 1 + rand(1000)
+
+puts solution # This statement is temporary while I test the game.
 puts "Guess what number I'm thinking of. It's between 1 and 1000"
 guess = gets.to_i
-while guess != answer
+while guess != solution
     puts "Wrong! Guess again!"
     guess = gets.to_i
 end 
