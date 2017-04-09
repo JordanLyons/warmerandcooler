bc78b2b374e50dc5a4162a8ee147c9f353784905
Jordan Lyons <JordanLyons@gmail.com>
2017-04-09 12:11:14 -0600

    Add if loop for warmer and cooler

--- warmercooler.rb
+++ warmercooler.rb
@@ -3,10 +3,19 @@ guesses = []
 puts solution # This statement is temporary while I test the game.
 puts "Guess what number I'm thinking of. It's between 1 and 1000"
 guess = gets.to_i
+guesses.push(guess)
 puts "Wrong! Guess again!" if guess != solution
 while guess != solution
-    guesses.push(guess)
-    puts "You've guessed #{guesses}" # Temporary, to be replaced by prompt comparing guesses.
     guess = gets.to_i
+    guesses.push(guess)
+    if guesses[-1] - solution < guesses[-2] - solution
+        puts "Warmer."
+    elsif guesses[-1] - solution > guesses[-2] - solution
+        puts "Cooler."
+    else
+        puts "Luke warm."
+    end
+    puts "You've guessed #{guesses}" # Temporary, to be replaced by prompt comparing guesses.
+    
 end 
     puts "You got it!"
