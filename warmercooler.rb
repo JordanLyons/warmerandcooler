ca607ba48b887740e94a124d863eec15bf8e7da5
Jordan Lyons <JordanLyons@gmail.com>
2017-04-09 14:27:36 -0600

    Move "Guess again" inside loop

--- warmercooler.rb
+++ warmercooler.rb
@@ -4,8 +4,9 @@ puts solution # This statement is temporary while I test the game.
 puts "Guess what number I'm thinking of. It's between 1 and 1000"
 guess = gets.to_i
 guesses.push(guess)
-puts "Wrong! Guess again!" if guess != solution
+puts "Wrong!" if guess != solution
 while guess != solution
+    puts "Guess again."
     guess = gets.to_i
     guesses.push(guess)
     if guesses[-1] - solution < guesses[-2] - solution
@@ -15,7 +16,6 @@ while guess != solution
     else
         puts "Luke warm."
     end
-    puts "You've guessed #{guesses}" # Temporary, to be replaced by prompt comparing guesses.
-    
+    puts "You've guessed #{guesses}" # Temporary, to be replaced by prompt comparing guesses.    
 end 
     puts "You got it!"
