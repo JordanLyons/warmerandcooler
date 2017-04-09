c03ac7cd5675f6f120acfeff338ff70cbb73ede5
Jordan Lyons <JordanLyons@gmail.com>
2017-04-09 11:29:20 -0600

    Create guesses array

--- warmercooler.rb
+++ warmercooler.rb
@@ -1,10 +1,12 @@
 solution = 1 + rand(1000)
-
+guesses = []
 puts solution # This statement is temporary while I test the game.
 puts "Guess what number I'm thinking of. It's between 1 and 1000"
 guess = gets.to_i
 while guess != solution
+    guesses.push(guess)
     puts "Wrong! Guess again!"
+    puts "You've guessed #{guesses}" # Temporary, to be replaced by prompt comparing guesses.
     guess = gets.to_i
 end 
     puts "You got it!"
