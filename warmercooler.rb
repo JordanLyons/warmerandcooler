f001b2fd2d7417a541320fd3cbfcfcc4d11ec1b9
Jordan Lyons <JordanLyons@gmail.com>
2017-04-09 11:03:22 -0600

    Add comment to puts statement

--- warmercooler.rb
+++ warmercooler.rb
@@ -1,5 +1,5 @@
 answer = 1 + rand(1000)
-puts answer
+puts answer # This statement is temporary while I test the game.
 puts "Guess what number I'm thinking of. It's between 1 and 1000"
 guess = gets.to_i
 while guess != answer
