434ed41f04a81781c8fc31edef1bbe92bd1b1cec
Jordan Lyons <JordanLyons@gmail.com>
2017-04-09 11:01:13 -0600

    Change unless to while loop

--- warmercooler.rb
+++ warmercooler.rb
@@ -1,8 +1,9 @@
 answer = 1 + rand(1000)
+puts answer
 puts "Guess what number I'm thinking of. It's between 1 and 1000"
 guess = gets.to_i
-unless guess == answer
+while guess != answer
     puts "Wrong! Guess again!"
-else
-    puts "You got it!"    
-end
+    guess = gets.to_i
+end 
+    puts "You got it!"
