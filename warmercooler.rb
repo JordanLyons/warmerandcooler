56a5b76d59ff9cdaedab7dc071708dddcfec2da5
Jordan Lyons <jordanlyons@gmail.com>
2017-05-05 21:22:00 +0000

    combine variable assignment and user input lines

--- warmercooler.rb
+++ warmercooler.rb
@@ -1,8 +1,8 @@
 solution_range = 100
 solution = 1 + rand(solution_range)
-guesses = []
+
 puts "Guess what number I'm thinking of. It's between 1 and #{solution_range}"
-guesses << gets.to_i
+guesses = [] << gets.to_i
 print guesses[0] != solution ? "Wrong!" : "Good guess!"
 while guesses[-1] != solution
     puts " Guess again."
